56bf7507a26b365cd64631bfd1b07a032007a990
Anne-gaelle BONNEFOY <abonnefo@made-f0Br6s2.clusters.42paris.fr>
2023-09-14 12:02:38 +0200

    free des tokens OK

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -6,13 +6,11 @@
 /*   By: abonnefo <abonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:20:20 by abonnefo          #+#    #+#             */
-/*   Updated: 2023/09/14 11:58:44 by abonnefo         ###   ########.fr       */
+/*   Updated: 2023/09/14 12:02:33 by abonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-// penser a free les tokens
 
 void	ft_free_tab(char **tab)
 {
@@ -31,7 +29,6 @@ void	ft_free_all_cmd(t_command *command)
 {
 	ft_free_tab(command->command_arg); //cat test.txt | wc | rev
 }
-
 
 void ft_free_tokens(t_token *head)
 {
